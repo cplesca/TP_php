94171ef81f4d9fb8a663ddc14b264fc7dddf4609
nsakhrao <sakhraoui.nassim@gmail.com>
2018-03-15 10:24:31 -0400

    je t'envoi le fichier data.php

---
+++ views/data.php
@@ -0,0 +1,116 @@
+<?php
+$berline = array(
+    'honda'=>array(
+        'model'=>"accord",
+        'image'=>"accord.jpg",
+        'km' => '140.000km',
+        'annee' =>'2014',
+        'prix'=>'20900$',
+    ),
+    'mercedes'=>array(
+        'model'=>"c203",
+        'image'=>"c203.jpg",
+        'km' => '100.000km',
+        'annee' =>'2010',
+        'prix'=>'19900$',
+    ),
+    'toyota'=>array(
+        'model'=>"corola",
+        'image'=>"corola.jpg",
+        'km' => '100.000km',
+        'annee' =>'2010',
+        'prix'=>'19900$',
+    ),
+    'hyunday'=>array(
+    'model'=>"elentra",
+    'image'=>"elentra.jpg",
+    'km' => '100.000km',
+    'annee' =>'2008',
+    'prix'=>'9900$',
+    ),
+    'kia'=>array(
+        'model'=>"forte",
+        'image'=>"forte.jpg",
+        'km' => '101.000km',
+        'annee' =>'2013',
+        'prix'=>'23900$',
+    )
+);
+$hatchback = array(
+    'honda'=>array(
+        'model'=>"civic",
+        'image'=>"civic.jpg",
+        'km' => '115.000km',
+        'annee' =>'2011',
+        'prix'=>'10900$',
+    ),
+    'mercedes'=>array(
+        'model'=>"class a",
+        'image'=>"classa.jpg",
+        'km' => '125.000km',
+        'annee' =>'2013',
+        'prix'=>'24900$',
+    ),
+    'toyota'=>array(
+        'model'=>"auris",
+        'image'=>"auris.jpg",
+        'km' => '140.000km',
+        'annee' =>'2009',
+        'prix'=>'10900$',
+    ),
+    'hyunday'=>array(
+        'model'=>"accent",
+        'image'=>"accent.jpg",
+        'km' => '118.000km',
+        'annee' =>'2008',
+        'prix'=>'7400$',
+    ),
+    'kia'=>array(
+        'model'=>"rio",
+        'image'=>"rio.jgg",
+        'km' => '112.000km',
+        'annee' =>'2008',
+        'prix'=>'6900$',
+    )
+);
+$suv = array(
+    'honda'=>array(
+        'model'=>"crv",
+        'image'=>"crv.jpg",
+        'km' => '135.000km',
+        'annee' =>'2013',
+        'prix'=>'13900$',
+    ),
+    'mercedes'=>array(
+        'model'=>"glk",
+        'image'=>"glk.jpg",
+        'km' => '125.000km',
+        'annee' =>'2015',
+        'prix'=>'29900$',
+    ),
+    'toyota'=>array(
+        'model'=>"rav4",
+        'image'=>"rav4.jpg",
+        'km' => '160.000km',
+        'annee' =>'2009',
+        'prix'=>'14900$',
+    ),
+    'hyunday'=>array(
+        'model'=>"tucson",
+        'image'=>"tucson.jpg",
+        'km' => '110.000km',
+        'annee' =>'2012',
+        'prix'=>'11900$',
+    ),
+    'kia'=>array(
+        'model'=>"sol",
+        'image'=>"soul.jpg",
+        'km' => '119.000km',
+        'annee' =>'2011',
+        'prix'=>'11900$',
+    )
+);
+
+
+
+
