98fb8d488d085f1643877904f88b2f1728974faf
kelgreen <raqguina@hotmail.com>
2018-03-23 02:16:27 -0400

    background video.

--- index.php
+++ index.php
@@ -2,8 +2,24 @@
 const PAGE_NAME = 'Accueil';
 require_once('views/page_top.php');
 ?>
+
+
+        <div class="container">
+            <div class="banner">
+                <div class="video-container">
+                    <video class="video" muted autoplay loop>
+                        <source src="videos/car.mp4" type="video/mp4">
+                        <source src="videos/car.webm" type="video/webm">
+                    </video>
+                    <div class="video-mask"></div>
+                </div>
+            </div>
+        </div>
     <main>
         <h1>AUTO PLUS</h1>
+
+
+
         <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
 
         <div class="row produtos">
