e79b0d127039637571e8d96498b2fc347a560351
kelgreen <raqguina@hotmail.com>
2018-03-23 00:20:19 -0400

    formulaire contact nous page, validation (prenom, nom, email) faite!

--- contact.php
+++ contact.php
@@ -1,30 +1,101 @@
 <?php
 const PAGE_NAME = 'Contact';
 require_once('views/page_top.php');
+
+$en_post = $_SERVER ['REQUEST_METHOD'] === 'POST';
+$validation = array(
+    'firstname' => array(
+        'is_valid' => false,
+        'value' => null,
+        'err_msg' => 'First Name have at lent 2 champs',
+    ),
+    'lastname' => array(
+        'is_valid' => false,
+        'value' => null,
+        'err_msg' => 'First Name have at lent 2 champs',
+    ),
+    'email' => array(
+        'is_valid' => false,
+        'value' => null,
+        'err_msg' => 'L\'adresse courriel n\'est pas valide',
+    ),
+
+);
+
+if ($en_post){
+
+    $validation['firstname']['value'] = filter_input(INPUT_POST, 'firstname', FILTER_SANITIZE_STRING);
+    $validation['firstname']['is_valid'] = strlen($validation['firstname']['value']) >= 2;
+
+
+    $validation['lastname']['value'] = filter_input(INPUT_POST, 'lastname', FILTER_SANITIZE_STRING);
+    $validation['lastname']['is_valid'] = strlen($validation['lastname']['value']) >= 2;
+
+
+    $validation['email']['value'] = filter_input(INPUT_POST, 'email', FILTER_VALIDATE_EMAIL);
+    $validation['email']['is_valid'] = (false !== $validation['email']['value']);
+
+
+    $form_valid = $validation['firstname']['is_valid']
+        && $validation['lastname']['is_valid']
+        && $validation['email']['is_valid'];
+
+    if ($form_valid) {
+        header('Location: emailreceived .php?msg='.urlencode('Merci, vous êtes inscrits sur le site'));
+        exit;
+    }
+
+
+}
+
+
 ?>
 
     <main>
-        <h1>Make an appointment</h1>
+        <h1>Nous joindre</h1>
         <div itemscope itemtype="http://schema.org/Store" id="wrapcontact" class="row">
             <div id="formdiv">
-                <form action="process.php" method="post">
-
+                <form method="post">
                     <div id="filds" >
                         <fieldset>
                             <div>
-                                <label for="lname">Last Name</label>
-                                <input type="text" id="lname" name="lname" placeholder="Green" pattern="[A-Za-z]{3}">
+                                <label for="firstname">Firstname</label>
+                                <input type="text" name="firstname" id="firstname"  placeholder="Prénom"
+                                       class="<?= $en_post && ! $validation['firstname']['is_valid'] ? 'invalid' : '' ?>"
+                                       value="<?= $en_post ? $validation['firstname']['value'] : '' ?>"
+                                />
+                                <?php
+                                if ($en_post && ! $validation['firstname']['is_valid'] ) {
+                                    echo '<span>', $validation['firstname']['err_msg'] ,'</span>';
+                                }
+                                ?>
                             </div>
                             <div>
-                                <label for="fname">First Name</label>
-                                <input type="text" id="fname" name="fname" placeholder="Philip" pattern="[A-Za-z]{3}">
+                                <label for="lastname">Lastname</label>
+                                <input type="text" name="lastname" id="lastname" placeholder="Nom"
+                                       class="<?= $en_post && ! $validation['lastname']['is_valid'] ? 'invalid' : '' ?>"
+                                       value="<?= $en_post ? $validation['lastname']['value'] : '' ?>"
+                                />
+                                <?php
+                                if ($en_post && ! $validation['lastname']['is_valid'] ) {
+                                    echo '<span>', $validation['lastname']['err_msg'] ,'</span>';
+                                }
+                                ?>
                             </div>
                             <div>
                                 <label for="email">Email</label>
-                                <input type="text" id="email" name="mail" placeholder="exemple@exemple.com" pattern="[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,3}$">
+                                <input type="text" name="email" id="email" placeholder="Courriel"
+                                       class="<?= $en_post && ! $validation['email']['is_valid'] ? 'invalid' : '' ?>"
+                                       value="<?= $en_post ? $validation['email']['value'] : '' ?>"
+                                />
+                                <?php
+                                if ($en_post && ! $validation['email']['is_valid'] ) {
+                                    echo '<span>', $validation['email']['err_msg'] ,'</span>';
+                                }
+                                ?>
                             </div>
                             <div>
-                                <label for="store">Preferred Store</label>
+                                <label for="store">Sélectionnez votre magasin: </label>
                                 <input type="text" name="store" id="store" list="options-store">
                                 <datalist id="options-store">
                                     <option value="Montreal Downtown" >Montreal downtown, QC</option>
@@ -35,31 +106,30 @@ require_once('views/page_top.php');
                             </div>
                         </fieldset>
                         <fieldset id="field2">
-                            <label>Lorem ipsum dolor sit amet?</label>
+                            <label>Langue: </label>
                             <div class="clearfix labpad">
-                                <label class="radioform">Yes
+                                <label class="radioform">Français
                                     <input type="radio" checked="checked" name="radio">
                                     <span class="radiocheckmark"></span>
                                 </label>
-                                <label class="radioform">No
+                                <label class="radioform">Anglais
                                     <input type="radio" name="radio">
                                     <span class="radiocheckmark"></span>
                                 </label>
                             </div>
-                            <label>Comments</label>
+                            <label>Votre message: </label>
                             <div class="labpad">
-									<textarea name="message" rows="5" cols="35">Yours message goes here
+									<textarea name="message" rows="5" cols="35">
 									</textarea>
                             </div>
                             <div id="checkform">
-                                <label class="checkboxes">Lorem ipsum dolor sit amet?
+                                <label class="checkboxes">Je souhaite recevoir courriel de confirmation.
                                     <input type="checkbox" checked="checked">
                                     <span class="checkmark"></span>
                                 </label>
-                            </div>
                         </fieldset>
                     </div>
-                    <input type="submit" >
+                    <input type="submit" name="submit" value="Soumettre"/>
                 </form>
             </div>
         </div>
