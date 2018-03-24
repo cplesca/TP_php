d42f2ec0a9e17f96db180bf3915beca294c14aa5
kelgreen <raqguina@hotmail.com>
2018-03-23 21:36:10 -0400

    no message

--- emailreceived.php
+++ emailreceived.php
@@ -5,7 +5,7 @@ require_once('views/page_top.php');
 <main>
     <h1>MERCI POUR VOTRE CONTACT!</h1>
     <div id="imgthanks">
-        <img src="images/thanks"  alt="merci">
+        <img src="images/thanks.jpg"  alt="merci">
         <h2>Nous vous contactons au plus vite.</h2>
     </div>
 
